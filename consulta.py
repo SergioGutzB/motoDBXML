c4bd17e0b886614d48bb25da7e8c594b0b396fbf
SergioGutzb <sergut18@gmail.com>
2014-09-01 14:54:47 -0500

    Se paso el codigo a POO, hace lo mismo

--- consulta.py
+++ consulta.py
@@ -1,32 +1,46 @@
 from xml.dom import minidom
 
-def consulta(consulta):
-	cadena = consulta.split("where")
-	select = cadena[0].replace("select ","").strip().split(" ")
-	where = cadena[1].strip().split(" ")
+class mayadb:	
+	cadena = ""
+	select = []
+	where = []
 	lp, lr, lista,c = [],[],[],""
-	dom = minidom.parse("Banda.xml")
+	dom = ""
+	filedb = ""
+	consulta = ""
 
-	for e in where:
-		d = e.split("=")		
-		lp.append(d[0])
-		lr.append(d[1])		
+	def __init__(self,filedb):
+		self.filedb=filedb;
+		self.dom = minidom.parse(self.filedb)
 
-	for i in range(0,len(lp)):			
-		re = dom.getElementsByTagName(lp[i])
-		for j in range(0,len(re)):
-			if re[j].childNodes[0].nodeValue == lr[i]:
-				c+="\n"
-				for h in range(0,len(select)):
-					elements = dom.getElementsByTagName(select[h])
-					c+= elements[j].childNodes[0].nodeValue + " "		
-	print c + "\n"
+	def separar(self):
+		self.cadena = self.consulta.split("where")
+		self.select = self.cadena[0].replace("select ","").strip().split(" ")
+		self.where = self.cadena[1].strip().split(" ")
 
-def consultar():
-	print "\nLa consulta solo debe contener espacios y las etiquetas (select, where) deben ir en minpusculas."
-	print "Ejemplo: 'select nombre nacionalidad sitio_web ano where ano=1983 ano=1982'.\n"
-	clta =raw_input("Ingrese una consulta:\n")
+	def ejecutar(self):		
+		self.separar()
+		for e in self.where:
+			d = e.split("=")		
+			self.lp.append(d[0])
+			self.lr.append(d[1])		
+
+		for i in range(0,len(self.lp)):			
+			re = self.dom.getElementsByTagName(self.lp[i])
+			for j in range(0,len(re)):
+				if re[j].childNodes[0].nodeValue == self.lr[i]:
+					self.c+="\n"
+					for h in range(0,len(self.select)):
+						elements = self.dom.getElementsByTagName(self.select[h])
+						self.c+= elements[j].childNodes[0].nodeValue + " "		
+		print self.c + "\n"
+
+	def consultar(self):
+		print "\nLa consulta solo debe contener espacios y las etiquetas (select, where) deben ir en minpusculas."
+		print "Ejemplo: 'select nombre nacionalidad sitio_web ano where ano=1983 ano=1982'.\n"
+		self.consulta =raw_input("Ingrese una consulta:\n")
+		self.ejecutar()
 		
-	consulta (clta)
 
-consultar()		
+db=mayadb("Banda.xml")
+db.consultar()
